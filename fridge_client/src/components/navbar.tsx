b959ed0647ff22e818f757c81afbdd74dfdfbdcf
trompeterm <trompeterm@msoe.edu>
2026-01-03 00:05:37 -0600

    navbar ui

---
+++ fridge_client/src/components/navbar.tsx
@@ -0,0 +1,13 @@
+import './navbar.css'
+
+export default function Navbar() {
+    return (
+        <div className="navbar"> 
+            <p>EnergyAI</p>
+            <ul>
+                <li><button>Leaderboard</button></li>
+                <li><button>Login</button></li>
+            </ul>
+        </div>
+    )
+}
