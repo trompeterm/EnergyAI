704fe0618167f1388c43d4f7e0954730b61517dc
trompeterm <trompeterm@msoe.edu>
2026-01-07 21:06:19 -0600

    inventory uses api

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/get_inventory")
+async def get_inventory():
+    return {
+        "White Monster": 3,
+        "Peach Monster": 1,
+        "Red Bull": 9
+    }
--- fridge_client/src/pages/Main.tsx
+++ fridge_client/src/pages/Main.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import InventoryDisplay from "../components/Inventory"
 import InventoryItem from "../components/InventoryItem"
 import RecommendationBox from "../components/RecommendationBox"
@@ -5,6 +6,14 @@ import Recommendation from "../components/Recommendation"
 import './Main.css'
 
 const Main = () => {
+    const [inventory, setInventory] = useState<Record<string, number>>({});
+
+    useEffect(() => {
+        fetch('http://localhost:8000/get_inventory')
+            .then(response => response.json())
+            .then(data => setInventory(data));
+    }, []);
+
     return (
         <>
       <div className="button-div">
@@ -13,9 +22,9 @@ const Main = () => {
          <button>Restock</button>
       </div>
       <InventoryDisplay>
-        <InventoryItem name="Drink" quantity={1} img_path="src/assets/sample_img.png" />
-        <InventoryItem name="Drink" quantity={2} img_path="src/assets/sample_img.png" />
-        <InventoryItem name="Drink" quantity={3} img_path="src/assets/sample_img.png" />
+        {Object.entries(inventory).map(([name, quantity]) => (
+          <InventoryItem key={name} name={name} quantity={quantity} img_path="src/assets/sample_img.png" />
+        ))}
       </InventoryDisplay>
       <RecommendationBox>
         <Recommendation name="Drink 1" img_path="src/assets/sample_img.png" percentage={85} summary="You will like this drink a lot" />
